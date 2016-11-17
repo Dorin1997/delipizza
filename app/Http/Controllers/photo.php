94d160c36bb6e08a33087ce30a8936e9f1ee218d
Station3 <Station3@Lab215-Station3>
2016-11-17 16:24:09 +0200

    start upload photo v1

--- app/Http/Controllers/photo.php
+++ app/Http/Controllers/photo.php
@@ -1,5 +1,25 @@
 <?php
 
+class Photo_Controller extends Base_Controller
+{
+    public $restful=true;
+    
+    public function post_upload()
+            {
+                $input = Input::all();
+               
+                $rules=array(
+                    'photo' = 'required|image|max:500'
+                );
+           
+            $v =validator::make($inout,$rules);
+             if ($v->fails()){
+                 return Redirect::to('/')->with_errors($v);
+             }
+             $extension = File::extension($input['photo']['name']);
+             $directory=path('public').'uploads/';
+            } 
+}
 
 
 ?>
